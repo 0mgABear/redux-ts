fdd630adf692ca1b57300e490bce6dbf7c22193a
0mgABear <yeoyi94@gmail.com>
2023-10-24 22:37:27 +0800

    completion of section 19 and entire course

--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import { useActions } from "../hooks/useActions";
+import { useSelector } from "../hooks/useTypeSelector";
+
 const RepoList: React.FC = () => {
-  return <div></div>;
+  const [term, setTerm] = useState("");
+  const { searchRepo } = useActions();
+  const { data, error, loading } = useSelector(
+    (state: any) => state.repositories
+  );
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    searchRepo(term);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={term} onChange={(e) => setTerm(e.target.value)} />
+        <button>Search</button>
+        {error && <h3>{error}</h3>}
+        {loading && <h3>{loading}</h3>}
+        {!error &&
+          !loading &&
+          data.map((name: string) => {
+            <div key={name}>{name}</div>;
+          })}
+      </form>
+    </div>
+  );
 };
 
 export default RepoList;
